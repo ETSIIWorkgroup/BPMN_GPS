ce7853fbf275850cc7a523467fa72643171a9b65
Alejandro Fernández Trigo <alejandro@fertry.tech>
2022-01-11 17:36:13 +0100

    P9

---
+++ Prácticas/Práctica_9/gps-send-mails/src/main/java/org/camunda/bpmn/mails/SendMail.java
@@ -0,0 +1,42 @@
+package org.camunda.bpmn.mails;
+
+import java.util.logging.Logger;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+
+public class SendMail {
+	final static Logger LOGGER = Logger.getLogger(SendMail.class.getName());
+	
+	private static final String HOST = "smtp.gmail.com";
+	private static final String USER = "practicas.ibet@gmail.com";
+	private static final String PWD = "wfefbdwaczwifwip";
+	private static final int PORT = 465;
+	
+	public void sendMail(String mail, String user, String item, long amount) {
+		if(mail != null && !mail.isEmpty()) {
+			Email email = new SimpleEmail();
+			email.setStartTLSEnabled(true);
+			email.setSSLOnConnect(true);
+			email.setHostName(HOST);
+			email.setAuthenticator(new DefaultAuthenticator(USER, PWD));
+			email.setSmtpPort(PORT);
+			email.setStartTLSEnabled(true);
+			try {
+				email.setFrom(USER, "[GPS]");
+				email.setSubject("Confirmación - Cargo tarjeta");
+				email.setMsg("Estimado/a " + user + " <"+mail+">,"
+							+ "\n\nSe ha realizado el cargo a su tarjeta de crédito. "
+							+ "\n - Item: " + item + "\n - Monto: " + amount
+							+ "\n\nSaludos cordiales."
+							+ "\nEquipo técnico");
+				email.addTo(mail,user);
+				email.send();
+			}catch(Exception e) {
+				e.printStackTrace();
+				LOGGER.warning("No fue posible enviar el correo a la dirección " + mail 
+						+ " - " + e.getMessage());
+			}	}	}
+}
+
+
